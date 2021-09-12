c01884889be71f86e5258f1a58a83a16c38c5052
Lilan Manoj <lilan.emedia@gmail.com>
2021-09-12 23:10:03 +0530

    Files Module - Views added

---
+++ resources/js/Pages/Modules/Files/Index.vue
@@ -0,0 +1,28 @@
+<template>
+    <app-layout title="File - Index">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Search/View Files
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+
+    export default defineComponent({
+        components: {
+            AppLayout,
+        },
+    })
+</script>
